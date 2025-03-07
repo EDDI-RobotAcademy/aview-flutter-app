cb165525ea5c6f04bc8b24a9049fd8e5c2dcbc3b
왕환민 <mini_wang@naver.com>
2025-03-07 12:12:53 +0900

    feat:[왕환민] appbar 및 router 추가[AVFA-2]

---
+++ jobstick/lib/kakao_authentication/presentation/ui/kakao_login_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../common_ui/custom_app_bar.dart';
+import '../providers/kakao_auth_providers.dart';
+
+class KakaoLoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          // 배경 이미지
+          Image.asset(
+            'images/login_bg6.png', // 이미지 경로를 실제 프로젝트에 맞게 수정하세요.
+            fit: BoxFit.cover,
+          ),
+
+          // Foreground Content
+          CustomAppBar(
+            body: Consumer<KakaoAuthProvider>(
+              builder: (context, provider, child) {
+                if (provider.isLoading) {
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                return Center(
+                  child: provider.isLoggedIn
+                      ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "로그인 성공!",
+                        style: TextStyle(fontSize: 20, color: Colors.white),
+                      )
+                    ],
+                  )
+                      : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: provider.isLoading ? null : () => provider.login(),
+                        child: Text("카카오 로그인"),
+                      )
+                    ],
+                  ),
+                );
+              },
+            ),
+            title: 'Kakao Login',
+          ),
+        ],
+      ),
+    );
+  }
+}
